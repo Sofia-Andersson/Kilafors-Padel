30596c9ddde421aecd61a816a3064bd6492c0e28
Sofia Andersson <sofia.e.i.andersson@gmail.com>
2023-02-08 11:20:09 +0100

    Created pages and first version styling

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggle() {
+    this.classList.toggle("active")
+  }
+  
+  // Selects an HTML element, and calls a function which will be executed when the element is clicked.
+  document.getElementById("section1").onclick = toggle;
+  document.getElementById("section2").onclick = toggle;
+  document.getElementById("section3").onclick = toggle;
+  document.getElementById("section4").onclick = toggle;
+  document.getElementById("section5").onclick = toggle;
+  document.getElementById("section6").onclick = toggle;
+  document.getElementById("section7").onclick = toggle;
+  document.getElementById("section8").onclick = toggle;
+  document.getElementById("section9").onclick = toggle;
+  document.getElementById("section10").onclick = toggle;
+  document.getElementById("section11").onclick = toggle;
+  document.getElementById("section12").onclick = toggle;
